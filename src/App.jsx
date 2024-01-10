f2dc18560b055aee6b6bfe42e04fa5795e1b2150
jessiyang1996 <jessiyang1996@gmail.com>
2024-01-09 20:14:54 -0600

    Header Created and no errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 // in the server we originally did:
 // let express = require('express ')
 import Footer from './components/Footer/Footer';
 import Card from './components/Card/Card';
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
+    <Header />
     <Card />
     <Footer />
 
