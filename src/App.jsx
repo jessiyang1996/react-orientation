482fc731742b24645659f10f69f7338628bcb79a
jessiyang1996 <jessiyang1996@gmail.com>
2024-01-09 21:03:35 -0600

    after class completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,15 @@ import Card from './components/Card/Card';
 import Header from './components/Header/Header'
 
 function App() {
-
+  
   return (
     <>
+
     <Header />
+
     <Card />
+    <Card />
+
     <Footer />
 
     </>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,29 @@
+// To use states, we need hooks.
+// hooks - a small bit of behavior that we can use or 
+    // manage
+
+// import the state
+// react gives us access to built in hooks
+import { useState } from 'react';
+
 function Card() {
+    //      prop1  ,  prop 2
+    const [ myName, setMyName] = useState('Jessica');
+    // useState Function gives us back an Array
+
+    function clickChangeName() {
+        // set name to new name
+        // this is where setName comes into play
+        console.log("Test");
+        setMyName('JESSICA');
+    }
+    
     return (
     <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-        count is 0
-        </button>
+        {/* Create state to increment the count*/}
+        <button>count is 0</button>
+            <p>{myName}</p>
+            <button onClick={clickChangeName}>Change Name</button>
             <p>
                 Edit <code>src/App.jsx</code> and save to test HMR
             </p>
