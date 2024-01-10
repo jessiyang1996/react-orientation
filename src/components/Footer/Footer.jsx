bc184eb83478d12a6df1361b373ea845ae640a2b
jessiyang1996 <jessiyang1996@gmail.com>
2024-01-09 19:36:00 -0600

    created footer and counter component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+// all components should start with a CAPITAL LETTER!!!!
+function Footer() {
+    // all functions should have a return!!
+    return (
+        <p className="read-the-docs">
+        Jessica's Application!
+        </p>
+    );
+}
+// when you want to put class it should be "className"
+// it is a keyword in JS so this is used to avoid it.
+
+// original: modules.exports = Footer
+export default Footer;
